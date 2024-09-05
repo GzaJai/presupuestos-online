9d0ed39e21e1ed0295c4b3de43ea1fca3cc66eb7
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2024-09-05 13:13:50 -0300

    first commit

---
+++ presupuestos/src/layout/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='mx-auto text-center py-4 space-y-1'>
+        <h1 className='text-slate-700 font-bold text-4xl '>Presupuestador online</h1>
+        <h2 className='text-xs'>por Gonzalo Jaime</h2>
+    </div>
+    
+  )
+}
+
+export default Header
