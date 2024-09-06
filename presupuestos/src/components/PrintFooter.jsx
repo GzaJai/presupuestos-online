53389079bca6b0fdb387799629137eb760f73406
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2024-09-06 12:36:27 -0300

    adding PrintFooter

---
+++ presupuestos/src/components/PrintFooter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PrintFooter = () => {
+  return (
+    <footer class="left-0 w-full text-center py-4">
+        creado por Gonzalo Jaime
+    </footer>
+  )
+}
+
+export default PrintFooter
