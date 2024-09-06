0d84490533924c9acf5c7ffa9748306268d6c632
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2024-09-06 12:10:54 -0300

    extracting handleDownloadPdf

--- presupuestos/src/components/BugdetPrinter.jsx
+++ presupuestos/src/components/BugdetPrinter.jsx
@@ -1,7 +1,6 @@
 import React, { useRef, useState } from 'react';
-import jsPDF from 'jspdf';
-import html2canvas from 'html2canvas';
 import Printable from './Printable';
+import { downloadPdf } from '../utils/pdfUtils';
 
 function BugdetPrinter() {
     const contentRef = useRef(null);
@@ -10,40 +9,6 @@ function BugdetPrinter() {
     const handleIsReady = () => {
         setIsReady(!isReady)
     }
-
-    const handleDownloadPdf = () => {
-        if (contentRef.current) {
-            html2canvas(contentRef.current, { scale: 2 }).then(canvas => {
-                const imgData = canvas.toDataURL('image/png');
-                const pdf = new jsPDF('p', 'mm', 'a4');
-                const imgWidth = 210;
-                const pageHeight = 295;
-                const imgHeight = canvas.height * imgWidth / canvas.width;
-                const scaleFactor = .7;
-    
-                const scaledImgWidth = imgWidth / scaleFactor;
-                const scaledImgHeight = imgHeight / scaleFactor;
-                const scaledPageHeight = pageHeight / scaleFactor;
-    
-                const xOffset = (imgWidth - scaledImgWidth) / 2;
-                let heightLeft = scaledImgHeight;
-                let position = 0;
-    
-                pdf.addImage(imgData, 'PNG', xOffset, position, scaledImgWidth, scaledImgHeight);
-                heightLeft -= scaledPageHeight;
-    
-                while (heightLeft >= 0) {
-                    position = heightLeft - scaledImgHeight;
-                    pdf.addPage();
-                    pdf.addImage(imgData, 'PNG', xOffset, position, scaledImgWidth, scaledImgHeight);
-                    heightLeft -= scaledPageHeight;
-                }
-    
-                pdf.save('documento.pdf');
-            });
-        }
-    };
-    
 
     return (
         <div className='w-100 flex flex-col'>
@@ -58,7 +23,7 @@ function BugdetPrinter() {
             {isReady&&
             <button 
             className='bg-slate-600 self-center mt-10 p-3 rounded-lg text-white font-bold hover:bg-blue-600 hover:duration-150 '
-            onClick={handleDownloadPdf}>
+            onClick={()=>downloadPdf(contentRef)}>
                 Descargar Presupuesto
             </button>}
         </div>
