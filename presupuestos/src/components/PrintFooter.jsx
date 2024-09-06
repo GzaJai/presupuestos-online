ec2f6a15b5501dd238979337af0c8edd89f0425f
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2024-09-06 13:05:35 -0300

    adding info in PrintFooter

--- presupuestos/src/components/PrintFooter.jsx
+++ presupuestos/src/components/PrintFooter.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const PrintFooter = () => {
   return (
-    <footer class="left-0 w-full text-center py-4">
-        creado por Gonzalo Jaime
+    <footer class="left-0 w-full text-center py-4 self-center">
+        <h6 className='pb-10'>creado por Gonzalo Jaime</h6>
+        <img className='mx-auto' src="../../public/portfolio-qr.png" alt="" />
     </footer>
   )
 }
