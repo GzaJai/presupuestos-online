afdae7c6af0556188a9684e6c974f6795d851326
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2024-09-06 16:00:22 -0300

    fixing className attribute

--- presupuestos/src/components/PrintFooter.jsx
+++ presupuestos/src/components/PrintFooter.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PrintFooter = () => {
   return (
-    <footer class="left-0 w-full text-center py-4 self-center">
+    <footer className="left-0 w-full text-center py-4 self-center">
         <h6 className='pb-10'>creado por Gonzalo Jaime</h6>
         <img className='mx-auto' src="https://raw.githubusercontent.com/GzaJai/presupuestos-online/main/presupuestos/public/portfolio-qr.png" alt="" />
     </footer>
