e74ef758027dc9dbcf175448d1b2ccdb3e30cf97
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2024-09-06 11:06:10 -0300

    adding isReady
    adding an is ready feature

--- presupuestos/src/components/BugdetPrinter.jsx
+++ presupuestos/src/components/BugdetPrinter.jsx
@@ -1,45 +1,66 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import jsPDF from 'jspdf';
 import html2canvas from 'html2canvas';
+import Printable from './Printable';
 
-function BugdetPrinter( { children } ) {
+function BugdetPrinter() {
     const contentRef = useRef(null);
+    const [isReady, setIsReady] = useState(false)
+
+    const handleIsReady = () => {
+        setIsReady(!isReady)
+    }
 
     const handleDownloadPdf = () => {
         if (contentRef.current) {
-            html2canvas(contentRef.current).then(canvas => {
+            html2canvas(contentRef.current, { scale: 2 }).then(canvas => {
                 const imgData = canvas.toDataURL('image/png');
                 const pdf = new jsPDF('p', 'mm', 'a4');
-                const imgWidth = 210; // Ancho del papel en mm
-                const pageHeight = 295; // Alto del papel en mm
+                const imgWidth = 210;
+                const pageHeight = 295;
                 const imgHeight = canvas.height * imgWidth / canvas.width;
-                let heightLeft = imgHeight;
+                const scaleFactor = .7;
+    
+                const scaledImgWidth = imgWidth / scaleFactor;
+                const scaledImgHeight = imgHeight / scaleFactor;
+                const scaledPageHeight = pageHeight / scaleFactor;
+    
+                const xOffset = (imgWidth - scaledImgWidth) / 2;
+                let heightLeft = scaledImgHeight;
                 let position = 0;
-
-                pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
-                heightLeft -= pageHeight;
+    
+                pdf.addImage(imgData, 'PNG', xOffset, position, scaledImgWidth, scaledImgHeight);
+                heightLeft -= scaledPageHeight;
+    
                 while (heightLeft >= 0) {
-                    position = heightLeft - imgHeight;
+                    position = heightLeft - scaledImgHeight;
                     pdf.addPage();
-                    pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
-                    heightLeft -= pageHeight;
+                    pdf.addImage(imgData, 'PNG', xOffset, position, scaledImgWidth, scaledImgHeight);
+                    heightLeft -= scaledPageHeight;
                 }
-
+    
                 pdf.save('documento.pdf');
             });
         }
     };
+    
 
     return (
         <div className='w-100 flex flex-col'>
             <div ref={contentRef}>
-            { children }
+            <Printable show={isReady}/>
             </div>
+            <button 
+            className='bg-slate-600 self-center mt-10 p-3 rounded-lg text-white font-bold hover:bg-blue-600 hover:duration-150 '
+            onClick={handleIsReady}>
+                {isReady?'Editar Presupuesto':'Presupuesto Listo'}
+            </button>
+            {isReady&&
             <button 
             className='bg-slate-600 self-center mt-10 p-3 rounded-lg text-white font-bold hover:bg-blue-600 hover:duration-150 '
             onClick={handleDownloadPdf}>
                 Descargar Presupuesto
-            </button>
+            </button>}
         </div>
     );
 }
