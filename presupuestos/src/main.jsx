d8690de9dae70430665e7ec2b4d57d868d85021c
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2024-09-06 11:06:53 -0300

    moving BudgetPrinter

--- presupuestos/src/main.jsx
+++ presupuestos/src/main.jsx
@@ -3,13 +3,11 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import Header from './layout/Header.jsx'
 import './index.css'
-import BugdetPrinter from './components/BugdetPrinter.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BugdetPrinter>
-      <Header/>
-      <App />
-    </BugdetPrinter>
+    <Header/>
+    <App />
   </StrictMode>,
 )
