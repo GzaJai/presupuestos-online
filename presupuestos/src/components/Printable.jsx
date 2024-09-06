d0d4c10a916c4738aff9a75e332d8aba505aeca1
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2024-09-06 11:00:56 -0300

    creating Printable
    this is what shows in the pdf

---
+++ presupuestos/src/components/Printable.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import Table from './Table'
+import { getShortDate, getExpireDate } from '../utils/getTime'
+
+
+const Printable = ({ show }) => {
+
+  return (
+    <div className='flex flex-col items-center'>
+      {show&&
+      <div>
+        <div className='flex py-10 items-center gap-[2rem]'>
+          <h3 className=' text-2xl font-bold'>Presupuesto Libreria Silver</h3>
+          <img src="../../public/Imagen1.png" alt="" />
+        </div>
+        <div className='flex items-center justify-around gap-[2rem]'>
+          <h4 className='text-lg font-semibold pb-5'>Fecha: {getShortDate()}</h4>
+          <h4 className='text-lg font-semibold pb-5'>Válido hasta: {getExpireDate(15)}</h4>
+        </div>
+      </div>
+      
+      }
+      <Table done={show}/>
+    </div>
+  )
+}
+
+export default Printable
