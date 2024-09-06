7dd5ce5f01ef888849740092679060f8a0437b2a
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2024-09-06 16:28:45 -0300

    adding validation to img load

--- presupuestos/src/components/PrintFooter.jsx
+++ presupuestos/src/components/PrintFooter.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
 const PrintFooter = () => {
+
+  const handleImageError = (e) => {
+    e.target.onerror = null; // Prevenir un bucle infinito si la segunda imagen también falla
+    e.target.src = 'https://raw.githubusercontent.com/GzaJai/presupuestos-online/main/presupuestos/public/portfolio-qr.png'; // Ruta en línea de la imagen de respaldo
+  };
+
   return (
     <footer className="left-0 w-full text-center py-4 self-center">
         <h6 className='pb-10'>creado por Gonzalo Jaime</h6>
-        <img className='mx-auto' src="https://raw.githubusercontent.com/GzaJai/presupuestos-online/main/presupuestos/public/portfolio-qr.png" alt="" />
+        <img className='mx-auto' src="../../public/portfolio-qr.png" alt="" onError={handleImageError} />
     </footer>
   )
 }
