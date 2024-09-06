65abfa475386ff7a101b101fe330c17b41b4b0ae
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2024-09-06 13:16:59 -0300

    updating src urls

--- presupuestos/src/components/PrintFooter.jsx
+++ presupuestos/src/components/PrintFooter.jsx
@@ -4,7 +4,7 @@ const PrintFooter = () => {
   return (
     <footer class="left-0 w-full text-center py-4 self-center">
         <h6 className='pb-10'>creado por Gonzalo Jaime</h6>
-        <img className='mx-auto' src="../../public/portfolio-qr.png" alt="" />
+        <img className='mx-auto' src="https://github.com/GzaJai/presupuestos-online/blob/main/presupuestos/public/portfolio-qr.png" alt="" />
     </footer>
   )
 }
