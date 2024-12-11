a26d8f2999f3a10e06a0142da6ced978123b2657
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2024-12-11 13:41:26 -0300

    moving logic to App

--- presupuestos/src/App.jsx
+++ presupuestos/src/App.jsx
@@ -1,11 +1,47 @@
-import BugdetPrinter from './components/BugdetPrinter.jsx'
+import { useState } from "react"
+import Table from "./components/Table"
+import ClientInfoForm from "./components/ClientInfoForm"
+import { fillForm } from "./utils/pdfUtils";
 
 function App() {
+  const [rows, setRows] = useState([{id: 0}]);
+  const [total, setTotal] = useState(0);
+  const [clientData, setClientData] = useState({});
+  const [done, setDone] = useState(false)
   
+  const checkRowsFilled = () => {
+    rows.map((row) => {
+      if(!row.filled){
+        alert("Hay filas sin completar.")
+        return false
+      }
+    })
+    return true
+  }
+
+  const printPdf = () => {
+    if(checkRowsFilled){
+      fillForm(rows, total, clientData)
+    }
+  }
+  
+  const handleDone = () => {
+    setDone(!done)
+    console.log('done');
+    
+  }
 
   return (
     <>
-      <BugdetPrinter />
+    <div className="w-1/2 mx-auto p-5 pb-10 bg-blue-400 rounded-lg">
+      {done ? <ClientInfoForm setData={setClientData} /> : <Table rows={rows} rowsSetter={setRows} total={total} totalSetter={setTotal} />}
+    </div>
+      <div className="flex justify-center space-x-10 pt-[4rem]">
+        {
+          done && <button className="bg-orange-600 p-5 rounded-lg text-white font-bold text-xl hover:bg-blue-600 hover:duration-150" onClick={handleDone}>Volver</button>
+        }
+        <button className="bg-green-600 p-5 rounded-lg text-white font-bold text-xl hover:bg-blue-600 hover:duration-150" onClick={done ? printPdf : handleDone}>{done ? "Imprimir" : "Siguiente"}</button>
+      </div>
     </>
   )
 }
