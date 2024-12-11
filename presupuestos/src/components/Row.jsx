89ffc2e6c55bee757ae7a9e40a6cd54e15fec1a3
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2024-12-11 13:45:35 -0300

    adding checks and modifying main handler

--- presupuestos/src/components/Row.jsx
+++ presupuestos/src/components/Row.jsx
@@ -1,54 +1,51 @@
 import { useRef, useState } from "react"
 import '../../../presupuestos/src/index.css'
 
-const Row = ({ rowId, addRow, done, removeRow, getTableTotal }) => {
+const Row = ({ rowId, saveRow, removeRow, getTableTotal }) => {
 
     const [isFilled, setIsFilled] = useState(false)
-    const [total, setTotal] = useState('')
+    const [total, setTotal] = useState(0.0)
 
-    const inputRefQ = useRef(null)
-    const inputRefP = useRef(null)
+    const inputRefQuantity = useRef(null)
+    const inputRefPrice = useRef(null)
     const inputRefProduct = useRef(null)
 
-    const handleOnChange = (e) => {
-        if (e.target.value != ''){
-            if (!isFilled){
-                addRow()
-                setIsFilled(true)
-            }
-        }        
+
+    const checkFilledRow = () => {        
+        if (inputRefQuantity.current.value == '' || inputRefPrice.current.value == '' || inputRefProduct.current.value == ''){
+            return false
+        }
+        setIsFilled(true)
+        return true
     }
 
-    const handleTotal = () => {
-        const quantity = inputRefQ.current.value
-        const price = inputRefP.current.value
-
-        setTotal(price*quantity)
-        getTableTotal(rowId, price*quantity)
-    }
+    const handleOnChange = () => {
+        const quantity = parseFloat(inputRefQuantity.current.value) || 0;
+        const price = parseFloat(inputRefPrice.current.value) || 0;
+        const totalValue = quantity * price;
+    
+        setTotal(totalValue);
+        saveRow(rowId, inputRefProduct.current.value, quantity, price, totalValue, checkFilledRow());
+    };
 
     const handleClear = () => {
-        inputRefQ.current.value = null
-        inputRefP.current.value = null
+        inputRefQuantity.current.value = ''
+        inputRefPrice.current.value = ''
         inputRefProduct.current.value = ''
         setTotal(0)
     }
 
   return (
     <div 
-    className={`w-[90%] mx-auto flex gap-[.2rem] py-[.1rem]  ${done ? 'disabled' : ''}`}
+    className="w-[90%] mx-auto flex gap-[.2rem] py-[.1rem]"
     >
+        <span className="w-[2rem] text-center p-2 rounded bg-white">{rowId+1}</span>
         <input ref={inputRefProduct} className="input-center flex-1 p-2 rounded" type="text" placeholder="Producto" onChange={handleOnChange} />
-        <input ref={inputRefQ} className="input-center w-[8rem] p-2 rounded" type="number" placeholder="Cantidad" onChange={handleTotal}/>
-        <input ref={inputRefP} className="input-center w-[8rem] p-2 rounded" type="number" placeholder="Precio unitario" onChange={handleTotal}/>
+        <input ref={inputRefQuantity} className="input-center w-[8rem] p-2 rounded" type="number" placeholder="Cantidad" onChange={handleOnChange}/>
+        <input ref={inputRefPrice} className="input-center w-[8rem] p-2 rounded" type="number" placeholder="Precio unitario" onChange={handleOnChange}/>
         <span className="w-[8rem] p-2 bg-white rounded" type="number" disabled>{total!=0?'$'+total:''}</span>
 
-        {!done && (
-                rowId !== 1 ? (
-                    <span className="w-[2rem] bg-red-600 text-white text-2xl font-bold text-center rounded-xl" onClick={removeRow}>X</span>
-                ) : (
-                    <span className="w-[2rem] bg-red-600 text-white text-2xl font-bold text-center rounded-xl" onClick={handleClear}>X</span>
-                ))}
+        <span className="w-[2rem] bg-red-600 text-white text-2xl font-bold text-center rounded-xl" onClick={removeRow}>X</span>
     </div>
   )
 }
