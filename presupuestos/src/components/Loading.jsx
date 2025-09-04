b5702314191eefe0072fe2c2eb8928e8e9d6528f
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2025-09-04 13:56:33 -0300

    Update: new Login form

---
+++ presupuestos/src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className='mx-auto p-3 mt-[10rem] bg-yui w-2/6 rounded-lg text-center'>
+        <h2 className='font-museo p-12 text-4xl text-white font-bold'>YUI ONE</h2>
+        <p className='p-2 text-xl text-white font-bold'>Cargando...</p>
+    </div>
+  )
+}
+
+export default Loading
