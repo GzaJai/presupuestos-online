6e73687418abc0d8e22e064307d057cd8524f7fb
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2025-09-04 14:43:52 -0300

    Update: new Header component

--- presupuestos/src/layout/Header.jsx
+++ presupuestos/src/layout/Header.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
+import { logout } from '../utils/authUtils'
+import { Navigate, useNavigate } from 'react-router-dom'
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleLogoutBtn = () =>{
+      logout()
+      navigate("/login")
+  }
+
+
   return (
-    <div className='mx-auto text-center py-12 space-y-1'>
-        <h1 className='text-slate-700 font-bold text-4xl '>Presupuestador online</h1>
-        <h2 className='text-sm'>por Gonzalo Jaime</h2>
+    <div className='flex mx-auto text-center bg-yui justify-between items-center mb-[2rem]'>
+      <img src="../../public/isologo-bnw-removebg.png" className='w-auto h-[4rem] ml-[2rem]' alt="" />
+      <div>
+        <h1 className='text-white font-museo font-semibold text-3xl'>Presupuestos</h1>
+      </div>
+      <div>
+      <svg  xmlns="http://www.w3.org/2000/svg"   
+      fill="#FFFFFF" viewBox="0 0 24 24" className='w-[2.5rem] h-auto mr-[2rem]'>
+      {/* <!--Boxicons v3.0 https://boxicons.com | License  https://docs.boxicons.com/free--> */}
+      <path d="M4 6h16v2H4zM4 11h16v2H4zM4 16h16v2H4z"></path>
+      </svg>
+        <i ></i>
+      </div>
     </div>
-    
   )
 }
 
