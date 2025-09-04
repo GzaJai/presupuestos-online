43a9964c2be90d639068202990cacd56a568a512
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2025-09-04 13:49:52 -0300

    Styling: adding shadow themes

--- presupuestos/tailwind.config.js
+++ presupuestos/tailwind.config.js
@@ -12,6 +12,10 @@ export default {
         colors: {
         yui: '#FF3838', // le das un nombre personalizado
       },
+       boxShadow: {
+        'custom-external-blur': '0 0 80px -15px rgba(0,0,0,0.25)',
+        'custom-internal-blur': 'inset 0 0 6px 0 rgba(0,0,0,0.25)'
+      },
     },
   },
   plugins: [],
