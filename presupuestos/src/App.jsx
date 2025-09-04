e1af1a73dc2ee160f8b6dcfa9e5004e9f2f5d3f5
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2025-09-04 15:57:56 -0300

    Update: new App component

--- presupuestos/src/App.jsx
+++ presupuestos/src/App.jsx
@@ -33,16 +33,16 @@ function App() {
 
   return (
     <>
-    <div className="w-10/12 mx-auto p-5 pb-10 bg-blue-400 rounded-lg">
-      {done ? <ClientInfoForm setData={setClientData} /> : <Table rows={rows} rowsSetter={setRows} total={total} totalSetter={setTotal} />}
-    </div>
-      <div className="flex justify-center space-x-10 pt-[4rem]">
-        {
-          done && <button className="bg-orange-600 p-5 mb-10 rounded-lg text-white font-bold text-xl hover:bg-blue-600 hover:duration-150" onClick={handleDone}>Volver</button>
-        }
-        <button className="bg-green-600 p-5 mb-10 rounded-lg text-white font-bold text-xl hover:bg-blue-600 hover:duration-150" onClick={done ? printPdf : handleDone}>{done ? "Imprimir" : "Siguiente"}</button>
+      <div className="w-10/12 mx-auto p-5 pb-10 bg-custom-gray rounded-lg font-mukta">
+        {done ? <ClientInfoForm setData={setClientData} /> : <Table rows={rows} rowsSetter={setRows} total={total} totalSetter={setTotal} />}
       </div>
-    </>
+        <div className="flex justify-center space-x-10 pt-[4rem]">
+          {
+            done && <button className="bg-orange-600 p-5 mb-10 rounded-lg text-white font-bold text-xl hover:bg-blue-600 hover:duration-150" onClick={handleDone}>Volver</button>
+          }
+          <button className="btn-custom-confirm text-lg p-[1.25rem]" onClick={done ? printPdf : handleDone}>{done ? "Imprimir" : "Siguiente"}</button>
+        </div>
+      </>
   )
 }
 
