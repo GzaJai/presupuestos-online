812f8dabf9ba3d200bfe1e7a64d91c62cc070dbc
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2025-09-04 22:02:26 -0300

    Update: new Header

--- presupuestos/src/layout/Header.jsx
+++ presupuestos/src/layout/Header.jsx
@@ -1,28 +1,32 @@
 import React from 'react'
 import { logout } from '../utils/authUtils'
-import { Navigate, useNavigate } from 'react-router-dom'
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom'
+import NavMenu from './NavMenu';
 
-const Header = () => {
+const Header = ({ pageTitle }) => {
   const navigate = useNavigate();
+  const [navMenuOpen, setNavMenuOpen] = useState(false);
 
-  const handleLogoutBtn = () =>{
-      logout()
-      navigate("/login")
+  const handleLogoutBtn = () => {
+    logout()
+    navigate("/login")
   }
 
+  const handleOpenNavMenu = () => {
+    setNavMenuOpen(!navMenuOpen)
+  }
 
   return (
     <div className='flex mx-auto text-center bg-yui justify-between items-center mb-[2rem]'>
-      <img src="../../public/isologo-bnw-removebg.png" className='w-auto h-[4rem] ml-[2rem]' alt="" />
+      <img src="../../public/isologo-bnw-removebg.png" className='w-auto h-[4rem] ml-[2rem] hover:cursor-pointer' alt="YUI ONE logo" onClick={() => navigate("/")} />
       <div>
-        <h1 className='text-white font-museo font-semibold text-3xl'>Presupuestos</h1>
+        <h1 className='text-white font-museo font-semibold text-3xl'>{ pageTitle }</h1>
       </div>
       <div>
-      <svg  xmlns="http://www.w3.org/2000/svg"   
-      fill="#FFFFFF" viewBox="0 0 24 24" className='w-[2.5rem] h-auto mr-[2rem]'>
-      {/* <!--Boxicons v3.0 https://boxicons.com | License  https://docs.boxicons.com/free--> */}
-      <path d="M4 6h16v2H4zM4 11h16v2H4zM4 16h16v2H4z"></path>
-      </svg>
+        <div className='px-[1rem]'>
+          <NavMenu logoutHandler={handleLogoutBtn} />
+        </div>
         <i ></i>
       </div>
     </div>
