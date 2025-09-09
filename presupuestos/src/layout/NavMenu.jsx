4c452f7f51cd2dfdeb0ed2984cebbc0888eaf499
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2025-09-09 20:48:20 -0300

    Refactor: new budgets route

--- presupuestos/src/layout/NavMenu.jsx
+++ presupuestos/src/layout/NavMenu.jsx
@@ -38,7 +38,7 @@ const NavMenu = ({ logoutHandler }) => {
         </div>
         <nav className="flex flex-col p-4 space-y-4">
           <Link onClick={handleLinkPressed} to="/" className="text-custom-darkgray">Inicio</Link>
-          <Link onClick={handleLinkPressed} to="/new-budget" className="text-custom-darkgray">Presupuestos</Link>
+          <Link onClick={handleLinkPressed} to="/budgets" className="text-custom-darkgray">Presupuestos</Link>
           <Link onClick={handleLinkPressed} to="/clients" className="text-custom-darkgray">Clientes</Link>
           <span className="mt-[10rem]"><p onClick={() => logoutHandler()} className="text-yui-900 hover:cursor-pointer">Cerrar Sesión</p></span>
         </nav>
