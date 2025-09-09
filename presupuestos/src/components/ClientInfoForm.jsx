92f0c9268a6256035e55766970bd7d9c0606b339
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2025-09-09 20:29:56 -0300

    Refactor: modifying the component to make it reusable

--- presupuestos/src/components/ClientInfoForm.jsx
+++ presupuestos/src/components/ClientInfoForm.jsx
@@ -2,76 +2,128 @@ import React from "react";
 import { useRef } from "react";
 
 const ClientInfoForm = ({ setData }) => {
-  const clientCuitRef = useRef(null);
   const clientNameRef = useRef(null);
-  const clientIvaRef = useRef(null);
+  const clientCuitRef = useRef(null);
+  const clientDniRef = useRef(null);
   const clientAddressRef = useRef(null);
+  const clientCategoryRef = useRef(null);
+  const clientPhoneRef = useRef(null);
+  const clientEmailRef = useRef(null);
 
   const handleOnChange = () => {
     const clientData = {
-      cuit: clientCuitRef.current.value,
       name: clientNameRef.current.value,
-      iva: clientIvaRef.current.value,
+      cuit: clientCuitRef.current.value,
+      dni: clientDniRef.current.value,
       address: clientAddressRef.current.value,
+      category: clientCategoryRef.current.value,
+      phone: clientPhoneRef.current.value,
+      email: clientEmailRef.current.value
     };
 
     setData(clientData);
   };
 
   return (
-    <div className="w-1/2 mx-auto p-5 pb-10 bg-blue-400 rounded-lg flex flex-col text-center gap-4 font-semibold text-lg">
-      <label htmlFor="client-cuit" className="text-lg font-semibold">
-        CUIT del cliente:
-      </label>
-      <input
-        ref={clientCuitRef}
-        className="input-center flex-1 p-2 rounded"
-        type="number"
-        name="client-cuit"
-        placeholder="CUIT"
-        onChange={handleOnChange}
-      />
+      <div className="p-[2rem] mx-auto rounded-lg flex flex-col gap-3 font-semibold text-lg">
+         <div className="flex flex-col w-3/4 mx-auto">
+          <label htmlFor="client-cuit" className="text-sm font-semibold">
+            CUIT del cliente:
+          </label>
+          <input
+            ref={clientCuitRef}
+            className="text-sm text-center flex-1 rounded border-[1.2px] border-custom-gray"
+            type="text"
+            name="client-cuit"
+            placeholder="CUIT"
+            onChange={handleOnChange}
+          />
+        </div>
 
-      <label htmlFor="client-name" className="text-lg font-semibold">
-        Nombre / Razon Social:
-      </label>
-      <input
-        ref={clientNameRef}
-        className="input-center flex-1 p-2 rounded"
-        type="text"
-        name="client-name"
-        placeholder="Nombre / Razon Social"
-        onChange={handleOnChange}
-      />
+        <div className="flex flex-col w-3/4 mx-auto">
+          <label htmlFor="client-name" className="text-sm font-semibold">
+            Nombre / Razón Social:
+          </label>
+          <input
+            ref={clientNameRef}
+            className="text-sm text-center flex-1 rounded border-[1.2px] border-custom-gray"
+            type="text"
+            name="client-name"
+            placeholder="Nombre / Razón Social"
+            onChange={handleOnChange}
+          />
+        </div>
 
-      <label htmlFor="client-iva" className="text-lg font-semibold">
-        Condición frente al IVA:
-      </label>
-      <select
-        id="iva-conditions"
-        ref={clientIvaRef}
-        className="text-center flex-1 p-2 rounded"
-        name="client-iva"
-        placeholder="Condición"
-        onChange={handleOnChange}
-      >
-        <option value="Consumidor Final">Consumidor Final</option>
-        <option value="Exento">Exento</option>
-        <option value="Responsable Inscripto">Responsable Inscripto</option>
-      </select>
+        <div className="flex flex-col w-3/4 mx-auto">
+          <label htmlFor="client-dni" className="text-sm font-semibold">
+            DNI del cliente:
+          </label>
+          <input
+            ref={clientDniRef}
+            className="text-sm text-center flex-1 rounded border-[1.2px] border-custom-gray"
+            type="text"
+            name="client-dni"
+            placeholder="DNI"
+            onChange={handleOnChange}
+          />
+        </div>
 
-      <label htmlFor="client-addres" className="text-lg font-semibold">
-        Domicilio del cliente:
-      </label>
-      <input
-        ref={clientAddressRef}
-        className="input-center flex-1 p-2 rounded"
-        type="text"
-        name="client-addres"
-        placeholder="Domicilio"
-        onChange={handleOnChange}
-      />
-    </div>
+        <div className="flex flex-col w-3/4 mx-auto">
+          <label htmlFor="client-address" className="text-sm font-semibold">
+            Domicilio del cliente:
+          </label>
+          <input
+            ref={clientAddressRef}
+            className="text-sm text-center flex-1 rounded border-[1.2px] border-custom-gray"
+            type="text"
+            name="client-address"
+            placeholder="Domicilio"
+            onChange={handleOnChange}
+          />
+        </div>
+
+        <div className="flex flex-col w-3/4 mx-auto">
+          <label htmlFor="client-category" className="text-sm font-semibold">
+            Categoría del cliente:
+          </label>
+          <input
+            ref={clientCategoryRef}
+            className="text-sm text-center flex-1 rounded border-[1.2px] border-custom-gray"
+            type="text"
+            name="client-category"
+            placeholder="Categoría"
+            onChange={handleOnChange}
+          />
+        </div>
+
+        <div className="flex flex-col w-3/4 mx-auto">
+          <label htmlFor="client-phone" className="text-sm font-semibold">
+            Teléfono del cliente:
+          </label>
+          <input
+            ref={clientPhoneRef}
+            className="text-sm text-center flex-1 rounded border-[1.2px] border-custom-gray"
+            type="text"
+            name="client-phone"
+            placeholder="Teléfono"
+            onChange={handleOnChange}
+          />
+        </div>
+
+        <div className="flex flex-col w-3/4 mx-auto">
+          <label htmlFor="client-email" className="text-sm font-semibold">
+            Email del cliente:
+          </label>
+          <input
+            ref={clientEmailRef}
+            className="text-sm text-center flex-1 rounded border-[1.2px] border-custom-gray"
+            type="email"
+            name="client-email"
+            placeholder="Email"
+            onChange={handleOnChange}
+          />
+        </div>
+      </div>
   );
 };
 
