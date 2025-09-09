2be5bec3a9f83d0e4257c5e865e70b3e4d7e02e6
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2025-09-09 13:08:56 -0300

    Add: home menu navigation

---
+++ presupuestos/src/layout/HomeMenu.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { getBudget } from '../utils/authUtils'
+
+const HomeMenu = () => {
+  return (
+    <div className='w-[30rem] h-[20rem] p-[2rem] mx-auto shadow-custom-external-blur text-center rounded'>
+        <h4 className='font-museo font-medium text-2xl'>¡Bienvenido!</h4>
+        <nav className="flex flex-col p-4 mt-5 space-y-4 items-center justify-center">
+          <Link to="/new-budget" className="flex items-center justify-center w-2/3 h-[2rem] text-white font-medium bg-yui-900 rounded">Presupuestos</Link>
+          <Link to="/clients" className="flex items-center justify-center w-2/3 h-[2rem] text-white font-medium bg-yui-900 rounded">Clientes</Link>
+          <button onClick={() => getBudget()}>Test get budget</button>
+        </nav>
+    </div>
+  )
+}
+
+export default HomeMenu
