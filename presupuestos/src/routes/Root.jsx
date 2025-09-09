78465310bd5dc220f6b6ff45b629d0e65c055c03
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2025-09-09 13:11:53 -0300

    Add: routes management

---
+++ presupuestos/src/routes/Root.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router-dom"
+import { api } from "../utils/authUtils";
+import Login from "../auth/Login";
+import App from "../App";
+import Loading from "../components/Loading";
+import Header from "../layout/Header";
+import Register from "../components/Register";
+import ClientInfoForm from "../components/ClientInfoForm";
+import AppLayout from "../layout/AppLayout";
+import HomeMenu from "../layout/HomeMenu";
+import ListClients from "../clients/ListClients";
+
+const Root = () => {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const res = await api.get("/auth/me");
+        setUser(res.data);
+      } catch (err) {
+        setUser(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+    checkAuth();
+  }, []);
+
+  if (loading) return <Loading />;
+
+  return (
+    <Routes>
+        <Route             
+          path="/"
+          element={user 
+            ? <AppLayout /> 
+            : <Navigate to="/login" replace />}>
+
+          <Route index element={<HomeMenu />} />
+          <Route path="new-budget" element={<App />} />
+          <Route path="clients" element={<ListClients />}/>
+          <Route path="new-client" element={<ClientInfoForm />} />
+
+        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<><Header /><Register /></>} />
+    </Routes>
+  );
+};
+
+export default Root;
