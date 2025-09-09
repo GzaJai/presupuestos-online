e073d663b902edef9ab7c074fa1df0b51c29179d
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2025-09-09 20:28:35 -0300

    Feature: views to view and manage clients

---
+++ presupuestos/src/clients/ListClients.jsx
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from 'react'
+import { api } from '../utils/authUtils'
+import Loading from '../components/Loading'
+import { Link } from 'react-router-dom'
+
+const ListClients = () => {
+    const [clients, setClients] = useState([])
+    const [loading, setLoading] = useState(true)
+    
+    useEffect(() => {
+    
+        if (clients.length == 0) {
+            setClients(getClients())
+        }
+        
+    }, [])
+    
+    const getClients = async () => {
+        try {
+            const res = await api.get("client/list");
+            setLoading(false)
+            setClients(res.data)
+        } catch (err) {
+            console.error(err.response?.data || err.message);
+        }
+    }
+
+  if (loading) return <Loading />
+
+  return (
+    <div className='font-mukta flex flex-col'>
+        <h4 className='text-center text-2xl font-black'>MIS CLIENTES</h4>
+        <Link to={"/new-client"} className="my-[1rem] mx-auto p-2 w-[14rem] text-white text-center font-medium bg-yui-900 rounded" >Agregar nuevo cliente</Link>
+
+        <div className="p-4 space-y-4">
+            {!loading &&
+            clients.map((client) => (
+            <div
+                key={client.id}
+                className="border rounded-lg max-w-[50rem] mx-auto p-4 px-[4rem] shadow-custom-external-blur bg-white"
+            >
+                <h2 className="text-lg text-center font-bold mb-2">{client.name}</h2>
+                <div className="flex flex-col gap-2">
+                <div className="flex justify-between">
+                    <strong>DNI:</strong>
+                    <p>{client.dni}</p>
+                </div>
+                <div className="flex justify-between">
+                    <strong>CUIT:</strong>
+                    <p>{client.cuit}</p>
+                </div>
+                <div className="flex justify-between">
+                    <strong>Domicilio:</strong>
+                    <p>{client.address}</p>
+                </div>
+                <div className="flex justify-between">
+                    <strong>Categoría:</strong>
+                    <p>{client.category}</p>
+                </div>
+                <div className="flex justify-between">
+                    <strong>Teléfono:</strong>
+                    <p>{client.phone}</p>
+                </div>
+                <div className="flex justify-between">
+                    <strong>Email:</strong>
+                    <p>{client.email}</p>
+                </div>
+                </div>
+            </div>
+            ))
+            }
+            </div>
+        </div>
+  )
+}
+
+export default ListClients
---
+++ presupuestos/src/clients/NewClient.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import ClientInfoForm from '../components/ClientInfoForm'
+import { api } from '../utils/authUtils'
+import { toast, Toaster } from 'sonner'
+import { HttpStatusCode } from 'axios'
+
+const NewClient = () => {
+  const [clientData, setClientData] = useState()
+
+  const handleNewClient = async () => {
+    try {
+      const res = await api.post("client/add", clientData);
+      if (res.data.statusCodeValue == 200) {
+        toast.success("Cliente agregado exitosamente")
+      } else if (res.data.statusCodeValue == 409) {
+        toast.warning("El cliente ya esta registrado")
+      }
+
+      
+    } catch (err) {
+      console.error(err.response?.data || err.message);
+    }
+  }
+
+  return (
+    <div className='mx-auto rounded w-[40rem] shadow-custom-external-blur'>
+      <ClientInfoForm setData={setClientData} />
+      <div className='w-full flex flex-col items-center'>
+        <button onClick={handleNewClient} className='btn-custom-confirm p-2'>Agregar cliente</button>
+      </div>
+      <Toaster />
+    </div>
+
+  )
+}
+
+export default NewClient
