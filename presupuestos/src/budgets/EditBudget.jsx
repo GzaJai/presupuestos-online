5a3f7f906e3f6581d221d9420bd4cefe29c433c2
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2025-09-12 22:06:58 -0300

    Refactor: handleSave notificates if the budget was saved or not

--- presupuestos/src/budgets/EditBudget.jsx
+++ presupuestos/src/budgets/EditBudget.jsx
@@ -1,51 +1,86 @@
-import { useState } from "react"
-import ClientInfoForm from "../clients/ClientInfoForm"
-import { fillForm } from "../utils/pdfUtils";
+import { useState } from "react";
 import { toast, Toaster } from "sonner";
 import Table from "../components/Table";
+import SelectClientForm from "./SelectClientForm";
+import { formatRow, saveBudget } from "../utils/apiUtils";
 
 function EditBudget() {
-  const [rows, setRows] = useState([{id: 0}]);
+  const [rows, setRows] = useState([{ id: 0 }]);
   const [total, setTotal] = useState(0);
-  const [clientData, setClientData] = useState({});
-  const [done, setDone] = useState(false)
-  
+  const [clientId, setClientId] = useState({});
+  const [done, setDone] = useState(false);
+
   const checkRowsFilled = () => {
     rows.map((row) => {
-      if(!row.filled){
-        toast.warning("Hay filas sin completar.")
-        return false
+      if (!row.filled) {
+        return false;
       }
-    })
-    return true
-  }
+    });
+    return true;
+  };
 
-  const printPdf = () => {
-    if(checkRowsFilled){
-      fillForm(rows, total, clientData)
-    }
-  }
-  
   const handleDone = () => {
-    setDone(!done)
-    console.log('done');
+    const filled = checkRowsFilled();
+    if (!filled) {
+      toast.warning("Hay filas sin completar.");
+    } else {
+      setDone(!done);
+    }
+  };
+
+  const handleSave = async () => {
+
+    const budgetItems = rows.map(formatRow);
     
-  }
+    const budgetData = {
+      clientId: clientId,
+      total: total,
+      budgetItems: budgetItems,
+    };
+
+    saveBudget(budgetData)
+      .then((res) => {
+        console.log(res.data);
+        
+        if (res.status == 200) {
+          toast.success("Se creó el presupuesto");
+        } else {
+          toast.warning("No se pudo crear el presupuesto");
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        toast.warning("No se pudo guardar el presupuesto");
+      });
+  };
 
   return (
     <>
       <div className="w-10/12 mx-auto p-5 pb-10 rounded-lg font-mukta shadow-custom-external-blur border-[1px] border-custom-gray text-black">
-        {done ? <ClientInfoForm setData={setClientData} /> : <Table rows={rows} rowsSetter={setRows} total={total} totalSetter={setTotal} />}
+        {done ? (
+          <SelectClientForm setClientData={setClientId} />
+        ) : (
+          <Table
+            rows={rows}
+            rowsSetter={setRows}
+            total={total}
+            totalSetter={setTotal}
+          />
+        )}
       </div>
-        <div className="flex justify-center space-x-10 pt-[2rem]">
-          {
-            done && <button className="bg-orange-600 p-5 mb-10 rounded-lg text-white font-bold text-xl hover:bg-blue-600 hover:duration-150" onClick={handleDone}>Volver</button>
-          }
-        <button className="btn-custom-confirm text-lg p-[1.25rem]" onClick={done ? printPdf : handleDone}>{done ? "Imprimir" : "Siguiente"}</button>
+      <div className="flex justify-center space-x-10 pt-[2rem]">
+        {done ? (
+          <>
+            <button className="btn-custom-confirm bg-yui-900 p-2" onClick={handleDone}>Volver</button>
+            <button className="btn-custom-confirm p-2" onClick={handleSave}>Guardar presupuesto</button>
+          </>
+        ) : (
+        <button className="btn-custom-confirm p-2" onClick={handleDone}>Siguiente</button>
+        )}
       </div>
       <Toaster />
     </>
-  )
+  );
 }
 
-export default EditBudget
+export default EditBudget;
