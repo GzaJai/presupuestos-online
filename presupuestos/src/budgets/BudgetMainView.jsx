e311f4959e2a9446e77e3ab9aa896d5c7950642b
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2025-09-15 11:06:04 -0300

    Refactor: add header layout for budget list

--- presupuestos/src/budgets/BudgetMainView.jsx
+++ presupuestos/src/budgets/BudgetMainView.jsx
@@ -32,6 +32,12 @@ const BudgetMainView = () => {
         <h4 className='text-center text-2xl font-black'>MIS PRESUPUESTOS</h4>
         <Link to={"/new-budget"} className="my-[1rem] mx-auto p-2 w-[14rem] text-white text-center font-medium bg-yui-900 rounded" >Nuevo presupuesto</Link>
         <div className='p-4 space-y-4'>
+            <div className='flex justify-between rounded-lg max-w-[50rem] mx-auto p-4 px-[4rem] bg-white font-mukta font-bold text-lg'>
+                <p>N°</p>
+                <p>Cliente</p>
+                <p>Fecha</p>
+                <p>Monto</p>
+            </div>
             {!loading &&
                 budgets.map((budget) => (
                     <BudgetCard key={budget.id} budget={budget}/>
