b2b85f3cd9ce5cfd31aae89e2e8ed393e4af5a9c
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2025-09-18 13:17:58 -0300

    Styling: shows user name and lastname

--- presupuestos/src/layout/Header.jsx
+++ presupuestos/src/layout/Header.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import NavMenu from "./NavMenu";
 
-const Header = ({ pageTitle }) => {
+const Header = ({ pageTitle, userData }) => {
   const navigate = useNavigate();
   const location = useLocation();
   
@@ -20,18 +20,21 @@ const Header = ({ pageTitle }) => {
 
   return (
     <div className="flex mx-auto text-center bg-yui-900 justify-between items-center mb-[2rem]">
+      <div className="flex items-center gap-[4rem]">
       <img
         src="../../public/isologo-bnw-removebg.png"
         className="w-auto h-[4rem] ml-[2rem] hover:cursor-pointer"
         alt="YUI ONE logo"
         onClick={() => navigate("/")}
       />
-      <div>
         <h1 className="text-white font-museo font-semibold text-3xl">
           {pageTitle}
         </h1>
       </div>
-      <div>
+      <div className="flex items-center gap-[2rem]">
+        <p className="text-white font-mukta font-semibold text-lg">
+          {userData.name} {userData.lastname}
+        </p>
         <div className="px-[1rem]">
           <NavMenu logoutHandler={handleLogoutBtn} />
         </div>
