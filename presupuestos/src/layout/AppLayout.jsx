625005c6212fa4c27c571462cf77d87085c7f56e
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2025-09-19 16:35:27 -0300

    Refactor: add global Toaster component

--- presupuestos/src/layout/AppLayout.jsx
+++ presupuestos/src/layout/AppLayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './Header'
 import { Outlet } from 'react-router-dom'
+import { Toaster } from 'sonner'
 
 const AppLayout = ({ userData }) => {
   return (
     <>
         <Header userData={userData} pageTitle={"Presupuestos"}/>
         <Outlet />
+        <Toaster />
     </>
   )
 }
