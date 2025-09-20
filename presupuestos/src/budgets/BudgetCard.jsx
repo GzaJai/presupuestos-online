46d99e70c83379f5dcf8b01dadedeaec9d622a0a
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2025-09-19 21:12:29 -0300

    Refactor: only passes budget ass state for BudgetDetails

--- presupuestos/src/budgets/BudgetCard.jsx
+++ presupuestos/src/budgets/BudgetCard.jsx
@@ -4,19 +4,10 @@ import { Link } from "react-router-dom";
 
 
 function BudgetCard({ budget }) {
-  const [client, setClient] = useState("");
-
-  useEffect(() => {
-    const fetchName = async () => {
-      const name = await getClientById(budget.clientId, setClient);
-    };
-    fetchName();
-  }, [budget.clientId]);
-
   return (
-    <Link to={"/budget-details"} state={{ budget, client }} className='flex justify-between border rounded-lg max-w-[50rem] mx-auto p-4 px-[4rem] shadow-custom-external-blur bg-white'>
+    <Link to={"/budget-details"} state={{ budget }} className='flex justify-between border rounded-lg max-w-[50rem] mx-auto p-4 px-[4rem] shadow-custom-external-blur bg-white'>
       <p>{budget.id}</p>
-      <p>{client.name || "Cargando..."}</p>
+      <p>{budget.clientName}</p>
       <p>{budget.issueDate}</p>
       <p>${budget.total}</p>
     </Link>
