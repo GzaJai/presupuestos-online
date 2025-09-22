d0fb3f9174609efc6e6d4f1c94d94fb82a685700
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2025-09-22 13:43:59 -0300

    Refactor: implementing ModalProvider and setting Outlet as children

--- presupuestos/src/layout/AppLayout.jsx
+++ presupuestos/src/layout/AppLayout.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import Header from './Header'
 import { Outlet } from 'react-router-dom'
 import { Toaster } from 'sonner'
+import ModalProvider from '../components/modal/ModalProvider'
 
 const AppLayout = ({ userData }) => {
   return (
     <>
         <Header userData={userData} pageTitle={"Presupuestos"}/>
-        <Outlet />
+        <ModalProvider>
+          <Outlet />
+        </ModalProvider>
         <Toaster />
     </>
   )
